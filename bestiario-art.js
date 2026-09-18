d9a88802d0b0477e0704b36a179f2f3cf66c9511
miniseek-tech <miniseek@gmail.com>
2026-09-18 12:18:52 +0200

    Add Bandido artwork

--- bestiario-art.js
+++ bestiario-art.js
@@ -29,7 +29,8 @@ const ART={
   'Zharruk Quebrantahierro':'./assets/ChatGPT Image 18 sept 2026, 11_43_23.png?v=1',
   'Clakakr':'./assets/ChatGPT Image 18 sept 2026, 11_49_30.png?v=2',
   'Garra Pico':'./assets/123facf3-a84e-409e-bed6-f14218da393e.png?v=1',
-  'Estatua Animada':'./assets/5a859267-7f27-43b8-919a-628d51e55c4f.png?v=1'
+  'Estatua Animada':'./assets/5a859267-7f27-43b8-919a-628d51e55c4f.png?v=1',
+  'Bandido':'./assets/43be35ce-5480-4f3f-b0ae-b7b48ee60322.png?v=1'
 };
 function artFor(name){
   if(ART[name])return ART[name];
@@ -46,6 +47,7 @@ function artFor(name){
   if(n==='clakakr')return './assets/ChatGPT Image 18 sept 2026, 11_49_30.png?v=2';
   if(n==='garrapico')return './assets/123facf3-a84e-409e-bed6-f14218da393e.png?v=1';
   if(n==='estatuaanimada')return './assets/5a859267-7f27-43b8-919a-628d51e55c4f.png?v=1';
+  if(n==='bandido')return './assets/43be35ce-5480-4f3f-b0ae-b7b48ee60322.png?v=1';
   return null;
 }
 function applyArt(){
